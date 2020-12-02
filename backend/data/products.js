3f58a4fe60ced70d6837d253249d3b3d97bd9adc
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-12-02 13:56:25 +0000

    added temp server

---
+++ backend/data/products.js
@@ -0,0 +1,69 @@
+const products = [
+  // {
+  //   _id: '1',
+  //   name: 'Mitchell Street',
+  //   image: '/images/w1.jpg',
+  //   description: 'Mitchell Lanes, Glasgow, Evening',
+  //   category: 'Landscape'
+  // },
+  {
+    _id: '2',
+    name: 'Robert',
+    image: '/images/w2.jpg',
+    type: 'Portrait',
+    location: 'Glasgow',
+    category: 'Portrait',
+  },
+  // {
+  //   _id: '3',
+  //   name: 'Alyx',
+  //   image: '/images/w3.jpg',
+  //   description: 'Alyx out of focus, Glasgow, Portrait',
+  //   category: 'Portrait'
+  // },
+  // {
+  //   _id: '4',
+  //   name: 'Sittin in a wall',
+  //   image: '/images/w4.jpg',
+  //   description: 'Charles on a wall, Glasgow, Portrait',
+  //   category: 'Portrait'
+  // },
+  // {
+  //   _id: '5',
+  //   name: 'Kenny Iglis Composer',
+  //   image: '/images/w5.jpg',
+  //   description: 'Kenny Inglis, Glasgow, Portrait',
+  //   category: 'Portrait'
+  // },
+  {
+    _id: '6',
+    name: 'Adrien Barry',
+    image: '/images/w66.jpg',
+    type: 'Portrait',
+    location: 'Glasgow',
+    category: 'Portrait'
+  },
+  // {
+  //   _id: '7',
+  //   name: 'Gary Beadle',
+  //   image: '/images/w7.jpg',
+  //   description: 'Gary Beadle, Glasgow, Portrait',
+  //   category: 'Portrait'
+  // },
+  // {
+  //   _id: '8',
+  //   name: 'Kirsty',
+  //   image: '/images/w8.jpg',
+  //   description: 'Kirsty, Glasgow, Portrait',
+  //   category: 'Portrait'
+  // },
+  // {
+  //   _id: '8',
+  //   name: 'Kirsty',
+  //   image: '/images/w8.jpg',
+  //   description: 'Kirsty, Glasgow, Portrait',
+  //   category: 'Portrait'
+  // },
+]
+
+module.exports = products
