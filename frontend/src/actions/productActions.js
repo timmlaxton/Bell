628890938d0418ae3a3c5651db6fc9ed2b6582ea
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-12-03 15:03:39 +0000

    can dispatch product list to printscreen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -6,7 +6,7 @@ import {
   
   
 
-} from '../constants/productContants'
+} from '../constants/productConstants'
 
 export const listProducts = () => async (dispatch) => {
    try {
--- frontend/src/screens/PrintScreen.js
+++ frontend/src/screens/PrintScreen.js
@@ -1,19 +1,33 @@
-import React from 'react'
-import products from '../products'
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {listProducts} from '../actions/productActions'
 import Product from '../components/Product'
 import {Row, Col} from 'react-bootstrap'
 
 const PrintScreen = () => {
+  const dispatch = useDispatch()
+
+  const productList = useSelector(state => state.productList)
+  const { loading, error, products} = productList
+
+  
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [dispatch])
+  
+  
   return (
     <>
-      
-        <Row className="rowp"> 
-        {products.map(product => (
-          <Col className="Col" sm={4} md={4} lg={4} xl={4}>
-          <Product product={product} />
-          </Col >
-        ))}
-        </Row>
+      {loading ? <h2>Loading...</h2> : error ? <h3>{error}</h3> : 
+       <Row className="rowp"> 
+       {products.map(product => (
+         <Col className="Col" sm={4} md={4} lg={4} xl={4}>
+         <Product product={product} />
+         </Col >
+       ))}
+       </Row>
+      }
+       
       
     </>
   )
