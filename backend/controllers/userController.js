ae6ba5c1da023272cbdca0b747026f41b1ecb866
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-12-04 14:10:33 +0000

    set user registration route controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,6 +25,41 @@ const authUser = asyncHandler(async (req, res) => {
 
 })
 
+// Register a new user 
+// POST /api/users/
+// Public
+const registerUser = asyncHandler(async (req, res) => {
+  const {name, email, password} = req.body
+
+ const userExists = await User.findOne({email})
+
+ if(userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+ }
+
+ const user = await User.create({
+   name,
+   email,
+   password
+ })
+
+ if(user) {
+   res.status(201).json({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin,
+    token: generateToken(user._id),
+   })
+ } else {
+   res.status(400)
+   throw new Error('Invalid user data')
+ }
+
+})
+
+
 // Get user profile
 // GET /api/users/profile
 // Private
@@ -47,5 +82,5 @@ if(user) {
 })
  
 export {
-  authUser, getUserProfile
+  authUser, registerUser, getUserProfile 
 }
